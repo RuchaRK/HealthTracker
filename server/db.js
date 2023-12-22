b1ddc2c566747dbd425cf1b5381562e913d3c874
ruchaRK <49073201+RuchaRK@users.noreply.github.com>
2023-12-22 10:04:45 +0000

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 // Access your MongoDB connection string from secrets
-const mongoURI = process.env.MONGODB;
+const mongoURI =
+  process.env.MONGODB ??
+  "mongodb+srv://neoGStudent:n1g8Vc3rpTCnoz1s@rucha-neog.kor0joq.mongodb.net/?retryWrites=true&w=majority";
 console.log(mongoURI);
 mongoose
   .connect(mongoURI, {
